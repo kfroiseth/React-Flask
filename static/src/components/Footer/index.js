161478dc36a411bf66dc3baf25e40caecf34419f
Froiseth <nvrmeant@ufl.edu>
2019-03-01 10:24:07 -0500

    edit footer

--- static/src/components/Footer/index.js
+++ static/src/components/Footer/index.js
@@ -10,13 +10,13 @@ export const Footer = () =>
                 <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                 <ul className="list-inline col-md-6">
                   <li><a href="docs" target="_blank">About This Site</a></li>
-                  <li><a href="http://www.ufl.edu/disability/" target="_blank">Disability Services</a></li>
-                  <li><a href="http://privacy.ufl.edu/privacystatement.html" target="_blank">Privacy Policy</a></li>
-                  <li><a href="mailto:apg-support@at.ufl.edu">Support</a></li>
-                  <li><a href="/Shibboleth.sso/Logout" target="_self">Logout</a></li>
+                  <li><a href="#" target="_blank">Services</a></li>
+                  <li><a href="#" target="_blank">Privacy Policy</a></li>
+                  <li><a href="#">Support</a></li>
+                  <li><a href="/Logout" target="_self">Logout</a></li>
                 </ul>
                 <div className="col-md-6 hidden-xs hidden-sm">
-				          <span>©2018</span> <a href="http://www.ufl.edu/">University of Florida</a>, Gainesville, FL 32611; (352) 392-3261
+				          <span>©2018</span> <a href="#">Sample React-Redux-Flask Template 
 			          </div>
                 </div>
             </div>
